c296edde49fbaa30af93d7373662607fadcb3612
jarek-janowski <jarjanowski95@gmail.com>
2021-03-26 19:24:26 +0100

    feat: add songs structure

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const port = process.env.PORT || 4041;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const songs = [{title: 'Ms. Jackson (San Holo Remix)', artist: 'Outkast'}]
-
+const songs = [{
+    id: '1',
+    title: 'Yesterday (ak9 & MIDIcal Remix)', 
+    artist: 'Borgeous & Zaeden',
+    audioUrl: 'https://www.dropbox.com/home/music?preview=Borgeous+%26+Zaeden+-+Yesterday+(ak9+%26+MIDIcal+Remix).mp3',
+    cover: 'https://i1.sndcdn.com/artworks-000147243714-60q2xq-t500x500.jpg',
+    
+}]
 
 app.get('/', (_, res) => {
     res.send('Express App');
@@ -36,41 +42,3 @@ app.post('/addsong', (req, res) => {
 app.listen(port, ()=>{
     console.log(`server is running on port: ${port}`);
 });
-
-
-// const express =require('express');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const port = process.env.PORT || 4041;
-
-// app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(bodyParser.json())
-
-// const users = [{name: 'Tony', email: 'tony@gmail.com'}]
-
-// app.get('/', (_, res) => {
-//     res.send('Express App');
-// });
-
-// app.get('/users', (_, res) => {
-//     res.json({ok: true, users});
-// });
-
-// app.get('/user/:name', (req, res) =>{
-//     const { name } = req.params;
-//     const user = users.filter((user) => user.name === name)[0];
-//     res.json({ ok: true, user });
-// })
-
-// app.post('/adduser', (req, res) => {
-//     const {name, email} = req.body;
-//     if(name && email){
-//         users.push({name, email});
-//         res.json({ok: true, users})
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log(`server is running on port: ${port}`)
-// });
